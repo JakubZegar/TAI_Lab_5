882d4e6b9a6eaa89d78e461f5c061968981e7dc8
Jakub Zegar <jakubzegar3@gmail.com>
2020-03-30 18:26:18 +0200

    Task 2

--- complex.js
+++ complex.js
@@ -1,32 +1,32 @@
-var Complex = /** @class */ (function () {
-    function Complex(real, imaginary) {
+"use strict";
+class Complex {
+    constructor(real, imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
-    Complex.prototype.add = function (obj) {
+    add(obj) {
         return new Complex(this.real + obj.real, this.imaginary + obj.imaginary);
-    };
-    Complex.prototype.sub = function (obj) {
+    }
+    sub(obj) {
         return new Complex(this.real - obj.real, this.imaginary - obj.imaginary);
-    };
-    Complex.prototype.mod = function () {
+    }
+    mod() {
         return Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imaginary, 2));
-    };
-    Complex.prototype.toString = function () {
+    }
+    toString() {
         if (this.imaginary >= 0) {
-            return this.real + "+" + this.imaginary + 'i';
+            return `${this.real}+${this.imaginary}` + 'i';
         }
         else {
-            return "" + this.real + this.imaginary + 'i';
+            return `${this.real}${this.imaginary}` + 'i';
         }
-    };
-    return Complex;
-}());
-var firstCplx = new Complex(2, 7);
-var secondCplx = new Complex(-4, 5);
-var sumResult = firstCplx.add(secondCplx);
-var subResult = firstCplx.sub(secondCplx);
-var modResult = firstCplx.mod();
+    }
+}
+let firstCplx = new Complex(2, 7);
+let secondCplx = new Complex(-4, 5);
+let sumResult = firstCplx.add(secondCplx);
+let subResult = firstCplx.sub(secondCplx);
+let modResult = firstCplx.mod();
 console.log(sumResult.toString());
 console.log(subResult.toString());
 console.log(modResult);
