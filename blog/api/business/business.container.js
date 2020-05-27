95d789d7160f23272f082f69042bc3df49ca011c
Jakub Zegar <jakubzegar3@gmail.com>
2020-05-27 19:40:13 +0200

    Lab finished

--- blog/api/business/business.container.js
+++ blog/api/business/business.container.js
@@ -3,21 +3,21 @@ import postManager from './post.manager'
 import userManager from './user.manager'
 
 function getContext(request) {
-    return { user: request && request.user };
+  return { user: request && request.user };
 }
 
 function getter(manager, request) {
-    return function () {
-        return manager.create(getContext(request), this);
-    };
+  return function (request) {
+    return manager.create(getContext(request), this);
+  };
 }
 
 const createBusinessContainer = (request, config) => {
 
-    return {
-        getPostManager: getter(postManager, request),
-        getUserManager: getter(userManager)
-    };
+  return {
+    getPostManager: getter(postManager, request),
+    getUserManager: getter(userManager, request)
+  };
 };
 
 export default createBusinessContainer;
--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,6 +1,12 @@
+import { LoginComponent } from './components/login/login.component';
+import { AuthService } from './services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from "@angular/common/http";
+import { HTTP_INTERCEPTORS} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {AuthInterceptor} from './services/auth.interceptor';
+import {DataService} from './services/data.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,16 +17,15 @@ import { BlogItemComponent } from './components/blog-item/blog-item.component';
 import { BlogItemTextComponent } from './components/blog-item-text/blog-item-text.component';
 import { BlogItemImageComponent } from './components/blog-item-image/blog-item-image.component';
 import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
-import { HttpClientModule } from '@angular/common/http';
-import { DataService } from './services/data.service';
 import { SummaryPipe } from './pipes/summary.pipe';
 import { SearchBarComponent } from './shared/search-bar/search-bar.component';
-import { FormsModule } from '@angular/forms';
 import { BlogHomeComponent } from './components/blog-home/blog-home.component';
 import { FilterPipe } from './pipes/filter.pipe';
 import { TextFormatDirective } from './directives/text-format.directive';
 import { SelectizeComponent } from './components/selectize/selectize.component';
 import { AddPostComponent } from './components/add-post/add-post.component';
+import { SignupComponent } from './components/signup/signup.component';
+import {AuthGuard} from "./services/auth.guard";
 
 @NgModule({
   declarations: [
@@ -40,9 +45,21 @@ import { AddPostComponent } from './components/add-post/add-post.component';
     TextFormatDirective,
     SelectizeComponent,
     AddPostComponent,
+    LoginComponent,
+    SignupComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-  providers: [DataService],
-  bootstrap: [AppComponent],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [DataService, AuthGuard, AuthService,  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }
+],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
